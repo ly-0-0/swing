0a7e3e3d790d4d910498c7e3c9a88281789fd2e7
ly-0-0 <74660321+ly-0-0@users.noreply.github.com>
2020-11-30 11:57:52 +0800

    Create 113001.继承父类.R.java

---
+++ 113001.继承父类.R.java
@@ -0,0 +1,10 @@
+package java1130;
+public class R {
+	static void lianxi(){
+		String tu_filrPath="img/tu1.gif";
+		String tuu_filrPath="img/tu2.gif";
+		int [][]array2d= {{1,2,3},{4,5,6}};
+		System.out.println(tu_filrPath+"\n"+tuu_filrPath+"\n"+array2d[1][1]);
+	}
+
+}
