195222974a380044c73e923fb2b248a94207c4b6
ly-0-0 <74660321+ly-0-0@users.noreply.github.com>
2020-11-30 12:03:14 +0800

    Create 113002.接口.A.java

---
+++ 113002.接口.A.java
@@ -0,0 +1,12 @@
+package java1130;
+
+public interface A {
+	static void lianxi(){
+		String tu_filrPath="img/tu1.gif";
+		String tuu_filrPath="img/tu2.gif";
+		int [][]array2d= {{1,2,3},{4,5,6}};
+		System.out.println(tu_filrPath+"\n"+tuu_filrPath+"\n"+array2d[0][2]);
+	}
+
+
+}
